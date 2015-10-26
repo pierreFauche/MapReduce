89a3b314de51138cca46b0f752176e2db84bb378
pierreFauche <pierre.fauche@gmail.com>
2015-10-26 14:09:30 +0100

    commit1

---
+++ mapReduce/src/main/java/com/projet/mapReduce/mapReduce/App.java
@@ -0,0 +1,14 @@
+package com.projet.mapReduce.mapReduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Coucou Pierre" );
+        System.out.println( "Coucou Pierre1" );
+    }
+}
