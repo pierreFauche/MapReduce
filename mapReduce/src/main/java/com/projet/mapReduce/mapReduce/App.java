40d4edcec6a9e8141d1adb8f6e03d8fdb32092e5
erloti <alexis.herlaut@gmail.com>
2015-10-26 16:13:15 +0100

    test:modification du chemin d'accès pour git.exe et du SDK

--- mapReduce/src/main/java/com/projet/mapReduce/mapReduce/App.java
+++ mapReduce/src/main/java/com/projet/mapReduce/mapReduce/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Coucou Pierre" );
         System.out.println( "Coucou Pierre1" );
         System.out.println( "Coucou Pierre2" );
+        System.out.println("Coucou Alexis");
+
     }
 }
